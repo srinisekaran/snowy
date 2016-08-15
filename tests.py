24e5e6fccc96911d84176027d54255ad17ebb07b
Srinivaas Sekaran <ssrinivaas@users.noreply.github.com>
2016-08-15 15:01:22 -0700

    Added tests
    Test-driven development skeleton

---
+++ tests.py
@@ -0,0 +1,16 @@
+import unittests
+from snowy import retrieve_news, retrieve_movies, retrieve_word_day
+
+class SnowyTest(unittest.TestCase):
+
+	def test_retrieve_news(self):
+		pass
+
+	def test_retrieve_movies(self):
+		pass
+
+	def test_retrieve_word_day(self):
+		pass
+
+if __name__ = '__main__':
+	unittest.main()
